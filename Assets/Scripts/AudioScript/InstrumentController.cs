f2ae7280e1b6a14e647b01741df2627306e2b0f2
Maude Beaulieu <maude.beaulieu.7@ulaval.ca>
2023-12-18 10:54:36 -0500

    musique dynamique - pitch et volume

--- Assets/Scripts/AudioScript/InstrumentController.cs
+++ Assets/Scripts/AudioScript/InstrumentController.cs
@@ -9,7 +9,6 @@ public class InstrumentController : MonoBehaviour
     public AudioMixer mixer;
     
     // clips for each instruments
-    public AudioClip[] BassClips; // 3
     public AudioClip[] PianoClips; // 4
     public AudioClip[] ViolinClips; // 3
     public AudioClip[] BeatClips; // 5
@@ -17,7 +16,6 @@ public class InstrumentController : MonoBehaviour
     public AudioClip[] PauseClips; // 3
 
     // tracks for each instrument
-    private AudioSource _bassClipsAudioSource; // 3
     private AudioSource _pianoClipsAudioSource; // 4
     private AudioSource _violinClipsAudioSource; // 3
     private AudioSource _beatClipAudioSources; // 5
@@ -25,7 +23,8 @@ public class InstrumentController : MonoBehaviour
     private List<AudioSource> _pauseAudioSources = new List<AudioSource>(); // 3
 
     // groups for easier editing of values
-    private AudioMixerGroup _percussionG;
+    private AudioMixerGroup _beatG;
+    private AudioMixerGroup _drumG;
     private AudioMixerGroup _pianoG;
     private AudioMixerGroup _violinG;
     private AudioMixerGroup _otherG;
@@ -41,6 +40,8 @@ public class InstrumentController : MonoBehaviour
     {
         InitializeMixerGroup();
         InitializeAudioSource();
+        ChangeVolume();
+        ChangePitch();
         
         PlayPiano(true);
         PlayViolin(true);
@@ -48,19 +49,19 @@ public class InstrumentController : MonoBehaviour
 
     private void InitializeMixerGroup()
     { 
-        _percussionG = mixer.FindMatchingGroups("Master/Instrument/Percussion")[0]; 
+        _beatG = mixer.FindMatchingGroups("Master/Instrument/Beat")[0]; 
         _pianoG = mixer.FindMatchingGroups("Master/Instrument/Piano")[0]; 
         _violinG = mixer.FindMatchingGroups("Master/Instrument/Violin")[0]; 
         _otherG = mixer.FindMatchingGroups("Master/Instrument/Other")[0]; 
+        _drumG = mixer.FindMatchingGroups("Master/Instrument/Drum")[0];
     }
 
     private void InitializeAudioSource()
     {
-        InitializeClipsAudio(BassClips, ref _bassClipsAudioSource, _percussionG);
         InitializeClipsAudio(PianoClips, ref _pianoClipsAudioSource, _pianoG);
         InitializeClipsAudio(ViolinClips, ref _violinClipsAudioSource, _violinG);
-        InitializeClipsAudio(BeatClips, ref _beatClipAudioSources, _percussionG);
-        InitializeClipsAudio(DrumClips, ref _drumClipsAudioSource, _percussionG);
+        InitializeClipsAudio(BeatClips, ref _beatClipAudioSources, _beatG);
+        InitializeClipsAudio(DrumClips, ref _drumClipsAudioSource, _drumG);
         InitializePauseClips();
     }
 
@@ -101,13 +102,17 @@ public class InstrumentController : MonoBehaviour
     public void PlayAll(bool aiming)
     {
         int musicLevel = GameManager.Instance.getMusicLevel();
+        
+        // set volumes for all instruments mixerGroup based on volume
+        
 
         if (level != musicLevel) // change of level
         {
             level = musicLevel;
+            ChangeVolume();
+            ChangePitch();
             PlayBeat(aiming, true);
             PlayDrum(aiming, true);
-            PlayBass(aiming, true);
             PlayPiano(true);
             PlayViolin(true);
         }
@@ -115,7 +120,6 @@ public class InstrumentController : MonoBehaviour
         {
             PlayBeat(aiming, false);
             PlayDrum(aiming, false);
-            PlayBass(aiming, false);
         }
     }
     
@@ -166,28 +170,6 @@ public class InstrumentController : MonoBehaviour
         }
     }
 
-    private void PlayBass(bool aiming, bool levelChange)
-    {
-        if (levelChange && level > 2)
-        {
-            _bassClipsAudioSource.Stop();
-            _bassClipsAudioSource.clip = BassClips[level-3];
-        }
-        
-        // in move and only if danger
-        if (!aiming && level > 2)
-        {
-            if (!_bassClipsAudioSource.isPlaying)
-            {
-                _bassClipsAudioSource.Play();
-            }
-        }
-        else
-        {
-            _bassClipsAudioSource.Stop();
-        }
-    }
-
     private void PlayPiano(bool levelChange)
     {
         if (level < 4 && levelChange)
@@ -233,18 +215,14 @@ public class InstrumentController : MonoBehaviour
         else
         {
             StopAudioSource(_pauseAudioSources);
+            PlayPiano(true);
+            PlayViolin(true);
         }
 
     }
 
-    public void ChangeIntensity(float intensity)
-    {
-        _violinClipsAudioSource.volume = intensity; // Adjust volume based on intensity
-    }
-
     public void StopAll()
     {
-        _bassClipsAudioSource.Stop();;
         _pianoClipsAudioSource.Stop();
         _violinClipsAudioSource.Stop();
         _beatClipAudioSources.Stop();
@@ -260,16 +238,67 @@ public class InstrumentController : MonoBehaviour
         }   
     }
 
-    public void SwitchSegment()
+    private void ChangeVolume()
     {
+        // start at 1, decrease by 0.3 -- 0 if level > 3
+        float pianoVolume = (level < 3) ? 1f - (0.3f * (level - 1)) : 0f;
         
-    }
+        // start at 0, increase by 0.2 -- 0 if level < 3
+        float violinVolume = (level > 2) ? (0.2f * level) : 0f;
+        
+        // increase by 0.2 every level
+        float beatVolume = 0.15f * level;
+        
+        // increase by 0.3 -- 0 if level = 1 or level = 5
+        float drumVolume = 0f;
+        if (level != 1 && level != 5)
+        {
+            drumVolume = 0.2f * (level - 1);
+        }
 
-    public void SetLevel(int l)
+        ChangeVolumeGroup(_pianoG.name, pianoVolume);
+        ChangeVolumeGroup(_violinG.name, violinVolume);
+        ChangeVolumeGroup(_beatG.name, beatVolume);
+        ChangeVolumeGroup(_drumG.name, drumVolume);
+    }
+    
+    // Change volume of the specified Mixer Group
+    private void ChangeVolumeGroup(string mixerGroupName, float volumeValue)
     {
-        level = l;
+        // Convert volume (0-1) to decibel range
+        float volumeDB = Mathf.Log10(volumeValue) * 20;
+        mixer.SetFloat(mixerGroupName + "_Volume", volumeDB); // "_Volume" is a parameter in the Mixer Group
     }
 
+    private void ChangePitch()
+    {
+        // start at 1.2, decrease by 0.2 -- 0 if level > 3
+        float pianoPitch = (level < 3) ? 1.0f - (0.2f * (level - 1)) : 1f;
+        
+        // start at 1, increase by 0.1 -- 0 if level < 3
+        float violinPitch = (level > 2) ? 0.8f + (0.1f * (level - 2)) : 0f;
+        
+        // start slow, increase after increase by 0.2 every level
+        float beatPitch = 0.5f + (level * 0.1f);
+        
+        // start at 0.5, increase by 0.2 -- 0 if level = 1 or level = 5
+        float drumPitch = 0.5f;
+        if (level != 1 && level != 5)
+        {
+            drumPitch = 0.5f + (0.1f * (level - 1));
+        }
+
+        ChangePitchGroup(_pianoG.name, pianoPitch);
+        ChangePitchGroup(_violinG.name, violinPitch);
+        ChangePitchGroup(_beatG.name, beatPitch);
+        ChangePitchGroup(_drumG.name, drumPitch);
+    }
+
+    private void ChangePitchGroup(string mixerGroupName, float pitchValue)
+    {
+        mixer.SetFloat(mixerGroupName + "_Pitch", pitchValue);
+    }
+    
     IEnumerator CrossFade(AudioSource channel, float targetVolume)
     {
         float time = 0;
